52486c61e7d1a45c655e76305451e5bc25642d14
Edward Dakin <ed.dakin@gmail.com>
2016-08-04 17:36:35 +0100

    Updates

--- mailer.php
+++ mailer.php
@@ -9,7 +9,7 @@
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        //$message = trim($_POST["message"]);
+        $message = 'Interested in:';
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -26,30 +26,50 @@
 		array_push($recipients, "comms@christchurchlondon.org");
 		if(isset($_POST['families-check'])) {
 			array_push($recipients, "amy@christchurchlondon.org");
+			$message .= 'Families, ';
+			
 		}
 		if(isset($_POST['prayer-check'])) {
-			array_push($recipients, "liam@christchurchlondon.org");
+			//array_push($recipients, "liam@christchurchlondon.org");
+			$message .= 'Prayer, ';
 		}
 		if(isset($_POST['production-check'])) {
 			array_push($recipients, "nate@christchurchlondon.org");
+			$message .= 'Production, ';
 		}
 		if(isset($_POST['tech-check'])) {
 			array_push($recipients, "nate@christchurchlondon.org");
+			$message .= 'Tech, ';
 		}
 		if(isset($_POST['welcome-check'])) {
 			array_push($recipients, "jo.wells@christchurchlondon.org");
+			$message .= 'Welcome, ';
 		}
 		if(isset($_POST['worship-check'])) {
 			array_push($recipients, "rich@christchurchlondon.org");
+			$message .= 'Worship, ';
+		}
+		
+		if(isset($_POST['south-check'])) {
+			$message .= 'south, ';
+		}
+		if(isset($_POST['central-check'])) {
+			$message .= 'central, ';
+		}
+		if(isset($_POST['westend-check'])) {
+			$message .= 'westend, ';
+		}
+		if(isset($_POST['east-check'])) {
+			$message .= 'east, ';
 		}
 		
         // Set the email subject.
         $subject = "$name... wants to TryServing";
-
+        
         // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-//        $email_content .= "Message:\n$message\n";
+		$email_content .= "Message:\n$message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
